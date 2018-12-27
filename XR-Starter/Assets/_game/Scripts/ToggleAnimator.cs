e7b6389d09b0ed40ed7a796972a8f39d78c3f73b
xwipeoutx <xwipeoutx@gmail.com>
2018-12-27 20:58:05 +1000

    MRTK and initial scene

---
+++ XR-Starter/Assets/_game/Scripts/ToggleAnimator.cs
@@ -0,0 +1,13 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class ToggleAnimator : MonoBehaviour, IInputClickHandler
+{
+    [SerializeField] Animator animator;
+    
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        animator.enabled = !animator.enabled;
+    }
+}
