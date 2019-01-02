d3a526a878020385718a00747255ca898c3fa7ed
Steve Leigh <steve.leigh@readify.net>
2019-01-02 16:07:07 +1000

    Master client now sends animator state when a player joins

--- XR-Starter/Assets/_game/Scripts/ToggleAnimator.cs
+++ XR-Starter/Assets/_game/Scripts/ToggleAnimator.cs
@@ -1,26 +1,59 @@
-﻿using HoloToolkit.Unity.InputModule;
+﻿using ExitGames.Client.Photon;
+using HoloToolkit.Unity.InputModule;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class ToggleAnimator : MonoBehaviour, IInputClickHandler
+public class ToggleAnimator : MonoBehaviour, IInputClickHandler, IInRoomCallbacks
 {
     [SerializeField] Animator animator;
     PhotonView view;
-    
+
     private void Awake()
     {
+        PhotonNetwork.AddCallbackTarget(this);
         view = GetComponent<PhotonView>();
     }
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        view.RPC(nameof(RPCToggle), RpcTarget.All, !animator.enabled);
+        var isEnabled = !animator.enabled;
+        SetAnimatorEnabled(isEnabled);
     }
-    
+
+    private void SetAnimatorEnabled(bool isEnabled)
+    {
+        view.RPC(nameof(RPCSetAnimatorEnabled), RpcTarget.All, isEnabled);
+    }
+
+    public void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            SetAnimatorEnabled(animator.enabled);
+        }
+    }
+
     [PunRPC]
-    void RPCToggle(bool isEnabled)
+    void RPCSetAnimatorEnabled(bool isEnabled)
     {
         animator.enabled = isEnabled;
+    }
+
+    public void OnPlayerLeftRoom(Player otherPlayer)
+    {
+    }
+
+    public void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+    {
+    }
+
+    public void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+    }
+
+    public void OnMasterClientSwitched(Player newMasterClient)
+    {
     }
 }
