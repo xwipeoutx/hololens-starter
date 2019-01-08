c9e19810fe840db7139682093923abb81da2ead6
xwipeoutx <xwipeoutx@gmail.com>
2019-01-08 22:49:55 +1000

    Adds a snake scene and state machine

---
+++ XR-Starter/Assets/_game/Prototype/Snake Snake/BackToIdle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToIdle : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("JustAte", false);
+        animator.SetBool("IsMoving", false);
+    }
+}
